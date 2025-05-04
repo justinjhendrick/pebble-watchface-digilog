b37cb1a2c63b52af0c1d91650c03fc2226ee678d
Justin Hendrick <justinjhendrick@gmail.com>
2025-05-03 17:11:50 -0700

    make it pretty. add week. add month. add lots

--- src/c/digilog.c
+++ src/c/digilog.c
@@ -11,85 +11,68 @@ static char s_buffer[BUFFER_LEN];
 static GPath* s_arc;
 
 static const GPathInfo ARC_POINTS = {
-  .num_points = 20,
+  .num_points = 40,
   .points = (GPoint []) {
-    {0, 0},
-    {0, 0},
-    {0, 0},
-    {0, 0},
-    {0, 0},
-    {0, 0},
-    {0, 0},
-    {0, 0},
-    {0, 0},
-    {0, 0},
-    {0, 0},
-    {0, 0},
-    {0, 0},
-    {0, 0},
-    {0, 0},
-    {0, 0},
-    {0, 0},
-    {0, 0},
-    {0, 0},
-    {0, 0},
-    {0, 0}
+    {0, 0}, {0, 0}, {0, 0}, {0, 0}, {0, 0}, {0, 0}, {0, 0}, {0, 0}, {0, 0}, {0, 0}, {0, 0},
+    {0, 0}, {0, 0}, {0, 0}, {0, 0}, {0, 0}, {0, 0}, {0, 0}, {0, 0}, {0, 0}, {0, 0}, {0, 0},
+    {0, 0}, {0, 0}, {0, 0}, {0, 0}, {0, 0}, {0, 0}, {0, 0}, {0, 0}, {0, 0}, {0, 0}, {0, 0},
+    {0, 0}, {0, 0}, {0, 0}, {0, 0}, {0, 0}, {0, 0}, {0, 0}, {0, 0}, {0, 0}, {0, 0}, {0, 0},
   }
 };
 
-static void draw_arc(GContext* ctx, GPoint center, int begin_deg, int arc_degs, int inner, int outer) {
+static void draw_arc_trigangle(GContext* ctx, GPoint center, int begin, int arc, int inner, int outer) {
   int half_points = ARC_POINTS.num_points / 2;
-  int step = arc_degs / (half_points - 1);
-  int end_deg = begin_deg + arc_degs;
+  int step = arc / (half_points - 1);
   for (int i = 0; i < half_points; i++) {
-    int a = begin_deg + step * i;
-    if (i + 1 == half_points) {
-      a = end_deg;
-    }
-    ARC_POINTS.points[i] = cartesian_from_polar(GPoint(0, 0), outer, a);
+    int a = begin + step * i;
+    ARC_POINTS.points[i] = cartesian_from_polar_trigangle(center, outer, a);
   }
   for (int j = 0; j < half_points; j++) {
-    int a = begin_deg + arc_degs - step * j;
-    if (j + 1 == half_points) {
-      a = begin_deg;
-    }
-    ARC_POINTS.points[half_points + j] = cartesian_from_polar(GPoint(0, 0), inner, a);
+    int a = begin + arc - step * j;
+    ARC_POINTS.points[half_points + j] = cartesian_from_polar_trigangle(center, inner, a);
   }
-  gpath_move_to(s_arc, center);
-  gpath_draw_outline(ctx, s_arc);
   gpath_draw_filled(ctx, s_arc);
+  gpath_draw_outline(ctx, s_arc);
 }
 
-static void draw_hour_text(GContext* ctx, struct tm* now, GPoint center, int radius, int size) {
-  // int minutes_per_hour_hand_rev = 60 * 24;
-  // int minutes_elapsed = now->tm_hour * 60 + now->tm_min;
-  // int hour_angle_deg = 360 * minutes_elapsed / minutes_per_hour_hand_rev + 180;
+static void draw_arc(GContext* ctx, GPoint center, int begin_deg, int arc_degs, int inner, int outer) {
+  draw_arc_trigangle(ctx, center, DEG_TO_TRIGANGLE(begin_deg), DEG_TO_TRIGANGLE(arc_degs), inner, outer);
+}
+
+static void draw_hour_text(GContext* ctx, struct tm* now, GPoint center, int radius, int sun_radius) {
   int hour_angle_deg = 360 * now->tm_hour / 24 + 180;
-  GFont font = fonts_get_system_font(FONT_KEY_GOTHIC_24_BOLD);
-  GSize bbox_size = GSize(size, size);
+  GFont font = fonts_get_system_font(FONT_KEY_GOTHIC_28_BOLD);
+  GSize bbox_size = GSize(sun_radius * 2, sun_radius * 2);
   GPoint bbox_mpoint = cartesian_from_polar(center, radius, hour_angle_deg);
   GRect bbox = rect_from_midpoint(bbox_mpoint, bbox_size);
   graphics_context_set_fill_color(ctx, GColorYellow);
   graphics_context_set_text_color(ctx, GColorBlack);
-  //graphics_fill_circle(ctx, bbox_mpoint, size / 2);
-  snprintf(s_buffer, BUFFER_LEN, "%d", now->tm_hour);
-  int arc_degs = 360 / 24;
-  draw_arc(ctx, center, hour_angle_deg - arc_degs / 2, arc_degs, radius - size / 2, radius + size / 2);
-  graphics_draw_text(ctx, s_buffer, font, bbox, GTextOverflowModeFill, GTextAlignmentCenter, NULL);
+  graphics_context_set_stroke_width(ctx, 3);
+  graphics_context_set_stroke_color(ctx, GColorBlack);
+  graphics_fill_circle(ctx, bbox_mpoint, sun_radius);
+  graphics_draw_circle(ctx, bbox_mpoint, sun_radius);
+  format_hour(s_buffer, BUFFER_LEN, now, true);
+  graphics_draw_text(ctx, s_buffer, font, vcenter(bbox), GTextOverflowModeTrailingEllipsis, GTextAlignmentCenter, NULL);
 }
 
-static void draw_min_text(GContext* ctx, struct tm* now, GPoint center, int radius, int size) {
+static void draw_min_text(GContext* ctx, struct tm* now, GPoint center, int radius, int outer_radius, int sun_radius) {
   int min_angle_deg = now->tm_min * DEG_PER_MIN;
-  GFont font = fonts_get_system_font(FONT_KEY_GOTHIC_24_BOLD);
-  GSize bbox_size = GSize(size, size);
+  GFont font = fonts_get_system_font(FONT_KEY_GOTHIC_28_BOLD);
+  GSize bbox_size = GSize(sun_radius * 2, sun_radius * 2);
   GPoint bbox_mpoint = cartesian_from_polar(center, radius, min_angle_deg);
   GRect bbox = rect_from_midpoint(bbox_mpoint, bbox_size);
   graphics_context_set_text_color(ctx, GColorBlack);
-  snprintf(s_buffer, BUFFER_LEN, "%d", now->tm_min);
-  graphics_draw_text(ctx, s_buffer, font, bbox, GTextOverflowModeFill, GTextAlignmentCenter, NULL);
+  if (now->tm_min < 10) {
+    snprintf(s_buffer, BUFFER_LEN, "0%d", now->tm_min);
+  } else {
+    snprintf(s_buffer, BUFFER_LEN, "%d", now->tm_min);
+  }
+  graphics_context_set_fill_color(ctx, GColorDarkGreen);
+  graphics_fill_circle(ctx, cartesian_from_polar(center, outer_radius - 5, min_angle_deg), 4);
+  graphics_draw_text(ctx, s_buffer, font, vcenter(bbox), GTextOverflowModeFill, GTextAlignmentCenter, NULL);
 }
 
-static void draw_ticks(GContext* ctx, int now, GPoint center, int radius, int tick_length, int from, int to, int by) {
+static void draw_ticks(GContext* ctx, GPoint center, int radius, int tick_length, int from, int to, int by) {
   for (int i = from; i < to; i += by) {
     int angle_deg = i * 360 / (to - from);
     graphics_draw_line(
@@ -100,22 +83,61 @@ static void draw_ticks(GContext* ctx, int now, GPoint center, int radius, int ti
   }
 }
 
-static void draw_sunlight_background(GContext* ctx, GRect bounds) {
-  GRect day = GRect(bounds.origin.x, bounds.origin.y, bounds.size.w, bounds.size.h * 4 / 10);
-  GRect twilight = GRect(bounds.origin.x, bounds.origin.y + day.size.h, bounds.size.w, bounds.size.h * 2 / 10);
-  GRect night = GRect(bounds.origin.x, twilight.origin.y + twilight.size.h, bounds.size.w, bounds.size.h * 4 / 10);
+static void draw_sunlight_background(GContext* ctx, GPoint center, int outer_radius) {
+  graphics_context_set_stroke_width(ctx, 3);
+  graphics_context_set_stroke_color(ctx, GColorBlack);
+  int inner_radius = 0;
+
+  // morning
+  graphics_context_set_fill_color(ctx, GColorMelon);
+  draw_arc(ctx, center, deg_from_mins(42), deg_from_mins(6), inner_radius, outer_radius);
+
+  // day
   graphics_context_set_fill_color(ctx, GColorVividCerulean);
-  graphics_fill_rect(ctx, day, 0, GCornerNone);
+  draw_arc(ctx, center, deg_from_mins(48), deg_from_mins(24), inner_radius, outer_radius);
 
-  graphics_context_set_fill_color(ctx, GColorCobaltBlue);
-  graphics_fill_rect(ctx, night, 0, GCornerNone);
-
+  // evening
   graphics_context_set_fill_color(ctx, GColorChromeYellow);
-  graphics_fill_rect(ctx, twilight, 0, GCornerNone);
+  draw_arc(ctx, center, deg_from_mins(12), deg_from_mins(6), inner_radius, outer_radius);
 
-  // graphics_context_set_stroke_width(ctx, 3);
-  // graphics_context_set_stroke_color(ctx, GColorBlack);
-  // graphics_draw_rect(ctx, twilight);
+  // night
+  graphics_context_set_fill_color(ctx, GColorCobaltBlue);
+  draw_arc(ctx, center, deg_from_mins(18), deg_from_mins(24), inner_radius, outer_radius);
+}
+
+static void draw_week(GContext* ctx, struct tm* now, GRect bounds, GPoint center, int vcr) {
+  graphics_context_set_stroke_color(ctx, GColorBlack);
+  graphics_context_set_stroke_width(ctx, 3);
+  graphics_context_set_text_color(ctx, GColorBlack);
+  static const char WEEK[8] = "SMTWTFS";
+  uint32_t trigangle_start = trigangle_from_mins(51);
+  uint32_t trigangle_step = trigangle_from_mins(18) / 7;
+  int inner_radius = vcr + 2;
+  int outer_radius = bounds.size.h - vcr;
+  int width = outer_radius - inner_radius;
+  GFont font = fonts_get_system_font(FONT_KEY_GOTHIC_24_BOLD);
+  for (int d = 0; d < 7; d++) {
+    if (now->tm_wday == d) {
+      graphics_context_set_fill_color(ctx, GColorWhite);
+    } else {
+      graphics_context_set_fill_color(ctx, GColorVividCerulean);
+    }
+    uint32_t trigangle_arc_begin = trigangle_start + trigangle_step * d;
+    draw_arc_trigangle(ctx, center, trigangle_arc_begin, trigangle_step, inner_radius, outer_radius);
+    uint32_t trigangle_arc_midpoint = trigangle_arc_begin + trigangle_step / 2;
+    GPoint text_center = cartesian_from_polar_trigangle(center, inner_radius + width / 2, trigangle_arc_midpoint);
+    GRect bbox = rect_from_midpoint(text_center, GSize(width, width));
+    s_buffer[0] = WEEK[d];
+    s_buffer[1] = '\0';
+    graphics_draw_text(ctx, s_buffer, font, vcenter(bbox), GTextOverflowModeWordWrap, GTextAlignmentCenter, NULL);
+  }
+}
+
+static void draw_month(GContext* ctx, struct tm* now, GPoint center, int outer_radius) {
+  GRect full_bbox = rect_from_midpoint(center, GSize(outer_radius * 2, outer_radius * 2));
+  GFont font = fonts_get_system_font(FONT_KEY_GOTHIC_24_BOLD);
+  format_day_and_month(s_buffer, BUFFER_LEN, now);
+  graphics_draw_text(ctx, s_buffer, font, vcenter(full_bbox), GTextOverflowModeWordWrap, GTextAlignmentCenter, NULL);
 }
 
 static void update_layer(Layer* layer, GContext* ctx) {
@@ -127,21 +149,25 @@ static void update_layer(Layer* layer, GContext* ctx) {
 
   GRect bounds = layer_get_bounds(layer);
   int vcr = min(bounds.size.h, bounds.size.w) / 2;
-  GPoint center = grect_center_point(&bounds);
-  int size = 34;
-  int between = vcr - size + 4;
-  draw_sunlight_background(ctx, bounds);
+  GPoint center = GPoint(vcr, bounds.origin.y + bounds.size.h - vcr);
+  int sun_radius = 17;
+  int between = vcr - sun_radius * 2;
+  draw_sunlight_background(ctx, center, bounds.size.h);
   graphics_context_set_fill_color(ctx, GColorWhite);
   graphics_fill_circle(ctx, center, between);
+  graphics_draw_circle(ctx, center, between);
 
-  graphics_context_set_stroke_width(ctx, 1);
+  graphics_context_set_stroke_width(ctx, 3);
   graphics_context_set_stroke_color(ctx, GColorWhite);
-  draw_ticks(ctx, now->tm_hour, center, between, 100, 1, 49, 2);
   graphics_context_set_stroke_color(ctx, GColorBlack);
-  draw_ticks(ctx, now->tm_min, center, between, -8, 0, 60, 5);
+  draw_ticks(ctx, center, between, -8, 0, 60, 5);
 
-  draw_hour_text(ctx, now, center, between + size / 2 + 1, size);
-  draw_min_text(ctx, now, center, between - size / 2 - 1, size);
+  int hour_radius = between + sun_radius;
+  draw_hour_text(ctx, now, center, hour_radius, sun_radius);
+  int min_radius = between - sun_radius * 3 / 2;
+  draw_min_text(ctx, now, center, min_radius, between, sun_radius);
+  draw_month(ctx, now, center, min_radius - sun_radius);
+  draw_week(ctx, now, bounds, center, vcr);
 }
 
 static void window_load(Window* window) {
--- src/c/utils.h
+++ src/c/utils.h
@@ -1,11 +1,15 @@
 #include <pebble.h>
 
-static GPoint cartesian_from_polar(GPoint center, int radius, int angle_deg) {
+static GPoint cartesian_from_polar_trigangle(GPoint center, int radius, int trigangle) {
   GPoint ret = {
-    .x = (int16_t)(sin_lookup(DEG_TO_TRIGANGLE(angle_deg)) * radius / TRIG_MAX_RATIO) + center.x,
-    .y = (int16_t)(-cos_lookup(DEG_TO_TRIGANGLE(angle_deg)) * radius / TRIG_MAX_RATIO) + center.y,
+    .x = (int16_t)(sin_lookup(trigangle) * radius / TRIG_MAX_RATIO) + center.x,
+    .y = (int16_t)(-cos_lookup(trigangle) * radius / TRIG_MAX_RATIO) + center.y,
   };
   return ret;
+}
+
+static GPoint cartesian_from_polar(GPoint center, int radius, int angle_deg) {
+  return cartesian_from_polar_trigangle(center, radius, DEG_TO_TRIGANGLE(angle_deg));
 }
 
 static GRect rect_from_midpoint(GPoint midpoint, GSize size) {
@@ -36,4 +40,49 @@ static void fast_forward_time(struct tm* now) {
   now->tm_mday = now->tm_sec % 31 + 1; /* Day. [1-31] */
   now->tm_mon = now->tm_sec % 12;      /* Month. [0-11] */
   now->tm_wday = now->tm_sec % 7;      /* Day of week. [0-6] */
+}
+
+static int deg_from_mins(int mins) {
+  return mins * 360 / 60;
+}
+
+static uint32_t trigangle_from_mins(int mins) {
+  return mins * TRIG_MAX_ANGLE / 60;
+}
+
+static void format_hour(char* buffer, int buffer_len, struct tm* now, bool force_12h) {
+  int hour = now->tm_hour;
+  if (force_12h || !clock_is_24h_style()) {
+    hour = now->tm_hour % 12;
+    if (hour == 0) {
+      hour = 12;
+    }
+  }
+  snprintf(buffer, buffer_len, "%d", hour);
+}
+
+static void format_day_of_week(char* buffer, int buffer_len, struct tm* now) {
+  strftime(buffer, buffer_len, "%a", now);
+}
+
+static void format_day_and_month(char* buffer, int buffer_len, struct tm* now) {
+  if (now->tm_mday / 10 == 1) {
+    strftime(buffer, buffer_len, "%b\n%eth", now);
+  } else if (now->tm_mday % 10 == 1) {
+    strftime(buffer, buffer_len, "%b\n%est", now);
+  } else if (now->tm_mday % 10 == 2) {
+    strftime(buffer, buffer_len, "%b\n%end", now);
+  } else if (now->tm_mday % 10 == 3) {
+    strftime(buffer, buffer_len, "%b\n%erd", now);
+  } else {
+    strftime(buffer, buffer_len, "%b\n%eth", now);
+  }
+}
+
+static void format_short_month(char* buffer, int buffer_len, struct tm* now) {
+  strftime(buffer, buffer_len, "%b", now);
+}
+
+static GRect vcenter(GRect bbox) {
+  return GRect(bbox.origin.x + 1, bbox.origin.y - 2, bbox.size.w, bbox.size.h);
 }
